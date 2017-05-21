53433d835605e97ea0b439e3a87a10c65a2fbaf5
OmarElEssiely <dev.omar@outlook.sa>
2017-05-21 12:17:32 +0200

    Blood Donation prototype
    prototype for blood donation system with client side technologies

---
+++ BloodDonationClientSide/js/MainBehavior.js
@@ -0,0 +1,90 @@
+﻿$(document).ready(function () {
+    $("#requestList").hide();
+    $("#NormalRequests").hide();
+    $("#UrgentRequests").hide();
+
+    ///////////// View hospital button to show hospital table
+    $("#ViewHospital").click(function () {
+        $("#tableHos").show();
+        $("#NormalRequests").hide();
+        $("#UrgentRequests").hide();
+    })
+
+    /////////////// slide down behavior //////////////////
+    $("input[name='option'][value ='Requests']").click(function () {
+        $('#requestList').slideToggle('slow');
+    })
+
+    ///////////////////normal request button to show table //////////////
+    $("#NrmBtn").click(function () {
+        $('#requestList').slideToggle('slow');
+        $("#tableHos").hide();
+        $("#NormalRequests").show();
+        $("#UrgentRequests").hide();
+    })
+
+    ///////////////////urgent request button to show table //////////////
+    $("#Urgbtn").click(function () {
+        $('#requestList').slideToggle('slow');
+        $("#tableHos").hide();
+        $("#NormalRequests").hide();
+        $("#UrgentRequests").show();
+    })
+    //----------------hospital data read -------------------//
+    $(function () {
+
+        $.getJSON("../js/Hospitals.json")
+         .success(function (data) {
+             $(data.hospitals).each(function (index, value) {
+                 var tr;
+                 tr = $('<tr/>');
+                 tr.append("<td>" + value.HospitalName + "</td>");
+                 tr.append("<td>" + value.city + "</td>");
+                 tr.append("<td>" + value.number + "</td>")
+                     .click(function () {
+                         newLocation(value.latitude, value.longitude);
+                     })
+                 $('#tableHos').append(tr);
+             })
+         })
+    });
+    //////////////////Normal Request data read /////////////
+    $(function () {
+        $.getJSON("../js/Requests.json")
+         .success(function (data) {
+             $(data.NormalRequest).each(function (index, value) {
+                 var tr;
+                 tr = $('<tr/>');
+                 tr.append("<td>" + value.Name + "</td>");
+                 tr.append("<td>" + value.PhoneNumber + "</td>");
+                 tr.append("<td>" + value.Hospital + "</td>");
+                 tr.append("<td>" + value.BTR + "</td>");
+                 tr.append("<td>" + value.RequestTime + "</td>").click(function () {
+                     newLocation(value.latitude, value.longitude);
+                 })
+
+                 $('#NormalRequests').append(tr);
+             })
+         })
+    });
+    /////////////////urgent Request data read /////////////////
+    $(function () {
+        $.getJSON("../js/Requests.json")
+         .success(function (data) {
+             $(data.UrgentRequest).each(function (index, value) {
+                 var tr;
+                 tr = $('<tr/>');
+                 tr.append("<td>" + value.Name + "</td>");
+                 tr.append("<td>" + value.PhoneNumber + "</td>");
+                 tr.append("<td>" + value.Hospital + "</td>");
+                 tr.append("<td>" + value.BTR + "</td>");
+                 tr.append("<td>" + value.RequestTime + "</td>").click(function () {
+                     newLocation(value.latitude, value.longitude);
+                 })
+
+                 $('#UrgentRequests').append(tr);
+             })
+         })
+    });
+    //////////////////---------End of Data Read ----------/////////
+})
---
+++ BloodDonationClientSide/js/MapjS.js
@@ -0,0 +1,66 @@
+﻿var map;
+var myLatLng;
+var marker;
+
+function initMap() {
+     myLatLng = { lat: -25.363, lng: 131.044 };
+    //var myLatLng;
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        panControl: true,
+        zoomControl: true,
+        mapTypeControl: true,
+        scaleControl: true,
+        streetViewControl: true,
+        overviewMapControl: true,
+        rotateControl: true,
+        zoom: 13
+    });
+
+}  //this function to intialize the map and show it
+
+if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            myLatLng = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            map.setCenter(myLatLng);
+
+            marker = new google.maps.Marker({
+                position: myLatLng,
+                map: map,
+                title: "your position"
+            });
+        })
+} //this function to get the currenct location of user and mark it
+
+function newLocation(newLat, newLng) {   //this function to change location
+    map.setCenter({
+        lat: newLat,
+        lng: newLng
+    });
+
+    var lating =
+    {
+        latit: newLat,
+        longit: newLng
+    };
+     marker = new google.maps.Marker({
+        position: new google.maps.LatLng(newLat, newLng),
+        map: map,
+    })
+}
+//function placeMarker(location) {
+//    if (marker == undefined){
+//        marker = new google.maps.Marker({
+//            position: new google.maps.LatLng(location),
+//            map: map, 
+//            animation: google.maps.Animation.DROP,
+//        });
+//    }
+//    else{
+//        marker.setPosition(location);
+//    }
+//    map.setCenter(location);
+//}
